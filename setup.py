9ec15cc4e07e182cd6fa6a916c20940aa86238aa
Dave <david.schuster@gmail.com>
2019-01-10 11:12:24 -0600

    removed requirement for time, sys, enum

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/circuitqed/nexstar",
     packages=setuptools.find_packages(),
-    install_requires=['sys','serial','datetime','pytz','time','enum'],
+    install_requires=['serial','pytz'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
